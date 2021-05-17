b497eb575ff172a918696ae57113dc55ab918b5c
superrexy <bintangrezeka@gmail.com>
2021-05-17 21:27:20 +0700

    Add Bootstrap, Jquery & Link on Index

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Learning</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!-- JQuery -->
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+
+    <!-- Poppins -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+
+</html>
